2e7290cfb7e7a564a2548a39bb082aefc854f5d5
pwam <gardineerw@gmail.com>
2014-02-27 01:09:29 -0500

    Added Function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+$(function(){
+        var zoom = new ZoomView('#zoom','#zoom :first');
+	var zoom2 = new ZoomView('#zoom2','#zoom2 :first');
+	var zoom3 = new ZoomView('#zoom3','#zoom3 :first');
+});
