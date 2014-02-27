f8e00b19e1980c5c5f5a4930ac70aa0bad497e43
pwam <gardineerw@gmail.com>
2014-02-27 01:02:20 -0500

    Added JQuery/Hammer plugin

---
+++ js/jquery.hammer.js
@@ -0,0 +1,17 @@
+jQuery.fn.hammer = function(options)
+{
+    return this.each(function()
+    {
+        var $el = jQuery(this);
+        var hammer = new Hammer(this, options);
+        var events = ['hold','tap','doubletap','transformstart','transform','transformend','dragstart','drag','dragend','release'];
+
+        for(var e=0; e<events.length; e++) {
+            hammer['on'+ events[e]] = (function($el, eventName) {
+                return function(ev) {
+                    $el.trigger(jQuery.Event(eventName, ev));
+                };
+            })($el, events[e]);
+        }
+    });
+};
